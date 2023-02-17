66ecb22be8e1acc52e28330e1815b125009dcef0
Rafa Lopes <rafalopessecond@gmail.com>
2023-02-17 10:27:21 +0000

    ForecastDayCard Accordion click radius
    Component width was set to 100%, having a click area much bigger that the component on larger screens

--- src/components/Weather/ForecastDayCard.jsx
+++ src/components/Weather/ForecastDayCard.jsx
@@ -32,54 +32,39 @@ function ForecastDayCard({
     const { lang, units } = useContext(LangContext);
     return (
         <Disclosure>
-            <Disclosure.Button className="w-full mx-auto">
-                <div className="flex flex-col space-y-3 w-full items-center justify-between bg-gray-700 text-gray-200 hover:text-gray-800 my-2 mx-auto p-3 px-8 rounded-lg md:min-w-[35rem] md:w-3/4 md:flex-row md:space-y-0  hover:bg-gray-200 transition ease-linear duration-300 ">
-                    <img src={"icons/" + icon + ".png"} alt="weather icon" />
+            <Disclosure.Button className="flex flex-col space-y-3 w-full items-center justify-between bg-gray-700 text-gray-200 hover:text-gray-800 my-2 mx-auto p-3 px-8 rounded-lg md:min-w-[35rem] md:w-3/4 md:flex-row md:space-y-0  hover:bg-gray-200 transition ease-linear duration-300 ">
+                <img src={"icons/" + icon + ".png"} alt="weather icon" />
 
-                    <h3 className="text-3xl font-bold ">
-                        {lang.weekdays[date.getDay()]}
-                    </h3>
+                <h3 className="text-3xl font-bold ">
+                    {lang.weekdays[date.getDay()]}
+                </h3>
 
-                    <span className="text-xl font-bold">
-                        <p>{temp + " " + units.temp}</p>
-                        <p>
-                            {lang.weather.feels_like +
-                                " " +
-                                feels_like +
-                                " " +
-                                units.temp}
-                        </p>
-                    </span>
-                </div>
+                <span className="text-xl font-bold">
+                    <p>{temp + " " + units.temp}</p>
+                    <p>
+                        {lang.weather.feels_like +
+                            " " +
+                            feels_like +
+                            " " +
+                            units.temp}
+                    </p>
+                </span>
             </Disclosure.Button>
-            <Disclosure.Panel className="w-full mx-auto">
-                {" "}
-                <div className="flex flex-col space-y-3 w-full md:min-w-[30rem] md:w-3/4 items-center justify-between bg-gray-700 text-gray-200 hover:text-gray-800 mb-2 mx-auto p-3 px-8 rounded-lg  md:flex-row md:space-y-0  hover:bg-gray-200 transition ease-linear duration-300 ">
-                    <h3 className="text-lg font-bold">{title}</h3>
-                    <p className="capitalize">{descp}</p>
+            <Disclosure.Panel className="flex flex-col space-y-3 w-3/4 md:min-w-[30rem] md:w-3/4 items-center justify-between bg-gray-700 text-gray-200 hover:text-gray-800 mb-2 mx-auto p-3 px-8 rounded-lg  md:flex-row md:space-y-0  hover:bg-gray-200 transition ease-linear duration-300 ">
+                <h3 className="text-lg font-bold">{title}</h3>
+                <p className="capitalize">{descp}</p>
 
-                    <span className="text-md md:border-l-2 border-gray-200  md:pl-7">
-                        <p>
-                            {lang.weather.temp_max +
-                                " " +
-                                temp_max +
-                                units.temp}
-                        </p>
-                        <p>
-                            {lang.weather.temp_min +
-                                " " +
-                                temp_min +
-                                units.temp}
-                        </p>
-                        <p>
-                            {lang.weather.feels_like +
-                                " " +
-                                feels_like +
-                                " " +
-                                units.temp}
-                        </p>
-                    </span>
-                </div>
+                <span className="text-md md:border-l-2 border-gray-200  md:pl-7">
+                    <p>{lang.weather.temp_max + " " + temp_max + units.temp}</p>
+                    <p>{lang.weather.temp_min + " " + temp_min + units.temp}</p>
+                    <p>
+                        {lang.weather.feels_like +
+                            " " +
+                            feels_like +
+                            " " +
+                            units.temp}
+                    </p>
+                </span>
             </Disclosure.Panel>
         </Disclosure>
     );
